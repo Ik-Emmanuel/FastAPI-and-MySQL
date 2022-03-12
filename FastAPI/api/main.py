275a3a5316bef57e23220663f4eee14d6a74852e
IK Emmanuel <47215749+Ik-Emmanuel@users.noreply.github.com>
2022-03-12 19:06:46 +0100

    modification: remote modifcation

--- FastAPI/api/main.py
+++ FastAPI/api/main.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import  List
 from fastapi import Depends, FastAPI, status, HTTPException
 from pydantic import BaseModel
 from .database import engine, SessionLocal
