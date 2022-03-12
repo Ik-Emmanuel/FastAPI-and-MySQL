22e38e48665a1a9d620982e44eb9bb729b3f91ba
Ikechukwu Emmanuel <nwokochaikechukwu13@gmail.com>
2022-03-12 19:09:12 +0100

    fix: Optional module imported on  main.py

--- FastAPI/api/main.py
+++ FastAPI/api/main.py
@@ -1,4 +1,4 @@
-from typing import  List
+from typing import  Optional, List
 from fastapi import Depends, FastAPI, status, HTTPException
 from pydantic import BaseModel
 from .database import engine, SessionLocal
